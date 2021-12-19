5d5749181523a36d763978be87a7b28d7acbb7d0
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-12-19 15:27:01 +0300

    add allure

--- src/test/java/netology/AuthTest.java
+++ src/test/java/netology/AuthTest.java
@@ -2,9 +2,13 @@ package netology;
 
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -16,6 +20,16 @@ import static netology.DataGenerator.getRandomPassword;
 
 
 class AuthTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
